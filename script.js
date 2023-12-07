6a2ae62dbccbbac495d9b5a0d50f6499db09df2f
Robin Abraham <robinabrahammathu@gmail.com>
2023-12-07 15:04:11 +0530

    Final commit to the project

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 // add class navbarDark on navbar scroll
 const header = document.querySelector('.navbar');
+  //Get the button
+  let mybutton = document.getElementById("btn-back-to-top");
 console.log(header)
 window.onscroll = function() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
     const top = window.scrollY;
     if(top >=100) {
         header.classList.add('navbarDark');
@@ -26,3 +36,10 @@ document.getElementById("mail_button").addEventListener("click", function() {
     var mailto = "mailto:akhilr2326@gmail.com?subject=" + subject + "&body=Name: " + name + "%0AEmail: " + email + "%0AMail Details: " + details;
     document.getElementById("mail_link").setAttribute("href", mailto);
   });
+
+  // When the user clicks on the button, scroll to the top of the document
+  mybutton.addEventListener("click", backToTop);
+  function backToTop() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+  }
